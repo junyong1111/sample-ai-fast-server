4147d4c542a5836b55493a57b1a5b8b1e7a3cce1
JunYongPark <jypark@hotseleer.co.kr>
2025-09-05 08:44:30 +0900

    feature : 회원가입 완료

--- src/app/user/repository.py
+++ src/app/user/repository.py
@@ -6,9 +6,23 @@ class UserRepository:
         self.logger = logger
         pass
 
-    async def get_user_by_id(
-            self, id: str, session):
-        return await session.get(User, id)
+    async def get_user_by_user_id(
+            self,
+            session,
+            user_id: str,
+            status: bool = True
+    ):
+        query = """
+            SELECT id, user_id
+            FROM user_master
+            WHERE user_id = $1
+                AND status = $2
+        """
+        return await session.fetchrow(query, user_id, status)
 
-    def create_user(self, user: User):
-        pass
+    async def create_user(self, session, user: User):
+        query = """
+            INSERT INTO user_master (user_id, password, name, memo, role, created_at)
+            VALUES ($1, $2, $3, $4, $5, NOW())
+        """
+        return await session.execute(query, (user.id, user.password, user.name, user.memo, user.role))
--- src/app/user/service.py
+++ src/app/user/service.py
@@ -1,6 +1,8 @@
+import hashlib
 from src.app.user.model import User
 from src.app.user.repository import UserRepository
 from src.config import settings
+from src.package.db import connection
 
 
 class UserService:
@@ -8,13 +10,14 @@ class UserService:
         self.logger = logger
         self.user_repository = UserRepository(logger)
 
-
-
-    # async def get_user_by_id(self, id: str):
-    #     return await self.user_repository.get_user_by_id(
-    #         id=id,
-    #         # session=session
-    #     )
+    async def get_user_by_user_id(self, user_id: str):
+        async with connection() as session:
+            user = await self.user_repository.get_user_by_user_id(
+                session=session,
+                user_id=user_id,
+            )
+        self.logger.info(f"유저 조회 결과: {user}")
+        return user
 
     async def create_user(self, user: User):
         self.logger.info(
@@ -26,4 +29,21 @@ class UserService:
             """
         )
         #1. 해당 유저 중복 확인
+        async with connection() as session:
+            user = await self.user_repository.get_user_by_user_id(
+                user_id=user.id,
+                session=session
+            )
+            if user:
+                raise Exception("이미 존재하는 유저입니다.")
+        #2. 유저 등록
+        hash_password = hashlib.sha256(user.password.encode()).hexdigest()
+        user.password = hash_password
+
+        async with connection() as session:
+            await self.user_repository.create_user(
+                session=session,
+                user=user
+            )
+
 
